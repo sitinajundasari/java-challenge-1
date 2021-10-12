9efd51b86ba66f64b20bea40e9780f369df1194a
sitinajundasari <91586288+sitinajundasari@users.noreply.github.com>
2021-10-12 21:48:23 +0700

    Add files via upload

---
+++ challenge biodata 1/biodata.java
@@ -0,0 +1,54 @@
+public class biodata {
+    public static void main(String[] args) {
+    
+        // String firstname;
+        // String lastname;
+        // String thirdname;
+        // String fullname;
+        // String address;
+        // int age;
+        // String favoritefood;
+        // string university;
+        // int nim;
+        // string course;
+        // String phonenumber;
+        // string hobby
+        // int weight;
+        // double height;
+    
+    
+        // firstname = "Siti";
+        // lastname = "Najunda"
+        // thirdname = "Sari"
+        // fullname = "Siti Najunda Sari"
+        // address = "Berastagi"
+        // age = "21 tahun"
+        // favoritefood = "Mie Goreng"
+        // university = "Universitas muhammadiyah sumatera utara"
+        // nim = "1805170183"
+        // course = "Accounting"
+        // phonenumber = "0895601798501"
+        // hobby = memasak
+        // weight = 58 kg;
+        // height = 165 cm;
+    
+    
+        System.out.println("firstname :" + "Siti" );
+        System.out.println("lastname :" + "Najunda" );
+        System.out.print("thirdname :" + "Sari");
+        System.out.print("fullname :" + "Siti Najunda Sari");
+        System.out.println("address :" + "Berastagi" );
+        System.out.println("age :" + "21 tahun" );
+        System.out.print("favoritefood :" + "Mie Goreng");
+        System.out.println("university :" + "universitas muhammadiyah sumatera utara" );
+        System.out.println("nim :" + "1805170183" );
+        System.out.println("course :" + "Accounting" );
+        System.out.println("phonenumber :" + "0895601798501" );
+        System.out.println("hobby :" + "memasak" );
+        System.out.println("weight :" + "58 kg" );
+        System.out.println("heigth :" + "165 cm" );
+     
+        
+    } 
+    
+    }
